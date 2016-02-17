134b57e5d038519a06455e883f004ec1ac7356ee
Joshua Johanan <josh359@gmail.com>
2016-02-16 22:36:40 -0500

    Added a navigation controller and view controller.
    Wired them up in the app delegate.

--- OurFirstApp/AppDelegate.swift
+++ OurFirstApp/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let firstViewController = FirstViewController()
+        let navController = RootNavigationController(rootViewController: firstViewController)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
         return true
     }
 
---
+++ OurFirstApp/RootNavigationController.swift
@@ -0,0 +1,42 @@
+//
+//  RootNavigationController.swift
+//  OurFirstApp
+//
+//  Created by Joshua Johanan on 2/16/16.
+//  Copyright © 2016 Joshua Johanan. All rights reserved.
+//
+
+import UIKit
+
+class RootNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationBar.barTintColor = UIColor(red: (0.0/255.0), green: (108.0/255.0), blue: (255.0/255.0), alpha: 1.0)
+        self.navigationBar.translucent = true
+        self.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        self.navigationBar.tintColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
